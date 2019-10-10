dbfc5ea7aee6701870307b976e5c5be092e81497
Angelo Leva <angelokta7@gmail.com>
2019-10-09 23:18:52 -0500

    Updates

---
+++ precache-manifest.a05fb4235428519f069f72e176937724.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cb3ed36b7b19612aabecbb98f2886ec",
+    "url": "/serpost/index.html"
+  },
+  {
+    "revision": "815769af21ad605671b8",
+    "url": "/serpost/static/css/main.672bd7e9.chunk.css"
+  },
+  {
+    "revision": "141aa0c710e65aa51edf",
+    "url": "/serpost/static/js/2.e57be45c.chunk.js"
+  },
+  {
+    "revision": "815769af21ad605671b8",
+    "url": "/serpost/static/js/main.56e97a72.chunk.js"
+  },
+  {
+    "revision": "f70b90dee0186470c015",
+    "url": "/serpost/static/js/runtime-main.45accb11.js"
+  },
+  {
+    "revision": "478652b2ead95c1eeff42f220678e034",
+    "url": "/serpost/static/media/icomoon.478652b2.ttf"
+  },
+  {
+    "revision": "993b3a35a419aaa2e1f022a611997970",
+    "url": "/serpost/static/media/icomoon.993b3a35.woff"
+  },
+  {
+    "revision": "bf4aa6966ad19c02b33c9ad15a90db5a",
+    "url": "/serpost/static/media/icomoon.bf4aa696.svg"
+  },
+  {
+    "revision": "c58bf31103fb4b9f8886de7057c833d3",
+    "url": "/serpost/static/media/icomoon.c58bf311.eot"
+  },
+  {
+    "revision": "f53ae75ffafced0581c1d51e3299d991",
+    "url": "/serpost/static/media/logo.f53ae75f.svg"
+  }
+]);
