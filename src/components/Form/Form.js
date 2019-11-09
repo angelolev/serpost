b632b1ff6950d66b4b00b125785612dd3bf84ae5
Angelo Leva <angelokta7@gmail.com>
2019-11-09 00:44:49 -0500

    added axios method for post package

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,19 +1,45 @@
 import React, { Component } from "react";
 import Select from "../Select/Select";
+import axios from "axios";
 
 class Form extends Component {
   handleSubmit(event) {
     event.preventDefault();
 
-    const product = {
-      origen: "SINGAPORE",
-      estado: "ENTREGADO",
-      tracking: "RB810114551SG",
-      destino: "ADMINISTRACION POSTAL JM",
-      tipoEnvio: "CERTIFICADOS TRADICIONALES",
-      aviso: "",
-      observacion: ""
-    };
+    axios
+      .post(
+        "http://clientes.serpost.com.pe/prj_online/Web_Busqueda.aspx/Consultar_Tracking",
+        {
+          params: {
+            Anio: 2019,
+            Filtro: 0,
+            Tracking: "RB810114551SG"
+          },
+          data: {
+            txtCorreo: "angelokta7@gmail.com",
+            txtContra: "gatoflaco10"
+          }
+        }
+      )
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
+      .finally(function() {
+        // always executed
+      });
+
+    // const product = {
+    //   origen: "SINGAPORE",
+    //   estado: "ENTREGADO",
+    //   tracking: "RB810114551SG",
+    //   destino: "ADMINISTRACION POSTAL JM",
+    //   tipoEnvio: "CERTIFICADOS TRADICIONALES",
+    //   aviso: "",
+    //   observacion: ""
+    // };
   }
 
   render() {
